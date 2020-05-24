165c4300f2faaec1daa94747008d0a13aaca96b6
fkubota <rkkubotay@gmail.com>
2020-05-24 18:09:33 +0900

    edit card in skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,7 +4,7 @@
       <h1 class="contents-title">Skills.</h1>
       <v-row align="center" justify="center">
         <v-col>
-          <v-card class="mx-auto" color="#26c6da" dark max-width="400" >
+          <v-card color="#f0f0f0"  hover=true height="300px">
             <v-card-title>
               <v-icon large left > mdi-twitter </v-icon>
               <span class="title font-weight-light">Twitter</span>
@@ -14,12 +14,55 @@
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col>
-          <v-card class="mx-auto" color="#26c6da" dark max-width="400" >
+        <v-col align="stretch">
+          <v-card color="#f0f0f0"  hover=true height="300px">
+            <v-card-title>
+              <v-icon large left > mdi-twitter </v-icon>
+              <span class="title font-weight-light">Twitterrrrrrr</span>
+            </v-card-title>
+          </v-card>
+        </v-col>
+        <v-col align="center">
+          <v-card color="#f0f0f0"  hover=true height="300px">
             <v-card-title>
               <v-icon large left > mdi-twitter </v-icon>
               <span class="title font-weight-light">Twitter</span>
             </v-card-title>
+            <v-card-text class="headline font-weight-bold">
+              ここにいろんなコメントを書くのだよ
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="center">
+        <v-col align="center">
+          <v-card color="#f0f0f0"  hover=true height="300px">
+            <v-card-title>
+              <v-icon large left > mdi-twitter </v-icon>
+              <span class="title font-weight-light">Twitter</span>
+            </v-card-title>
+            <v-card-text class="headline font-weight-bold">
+              ここにいろんなコメントを書くのだよ
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col align="center">
+          <v-card color="#f0f0f0"  hover=true height="300px">
+            <v-card-title>
+              <v-icon large left > mdi-twitter </v-icon>
+              <span class="title font-weight-light">Twitter</span>
+            </v-card-title>
+          </v-card>
+        </v-col>
+        <v-col align="center">
+          <v-card color="#f0f0f0"  hover=true height="300px">
+            <v-card-title>
+              <v-icon large left > mdi-twitter </v-icon>
+              <span class="title font-weight-light">Twitter</span>
+            </v-card-title>
+            <v-card-text class="headline font-weight-bold">
+              ここにいろんなコメントを書くのだよ
+            </v-card-text>
           </v-card>
         </v-col>
       </v-row>
