02d42ecad0ec7b90bade82846ab8ad47c873b776
fkubota <rkkubotay@gmail.com>
2020-05-25 21:02:02 +0900

    skillのcardにホバーを入れた

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,7 +4,13 @@
       <h1 class="contents-title">Skills.</h1>
       <v-row>
         <v-col cols=4 v-for="(item, key) in items" :key="key">
+          <v-hover v-slot:default="{ hover }">
           <v-card v-bind:color="skill_card_color" hover=true height="300px" class="mx-auto">
+        <v-expand-transition>
+        <div v-if="hover" class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text" style="height: 100%;" >
+            $14.99
+        </div>
+        </v-expand-transition>
             <v-card-title>
                 <v-icon large left> mdi-laptop </v-icon>
               <span class="title font-weight-light">{{ item.title }}</span>
@@ -13,6 +19,7 @@
               {{ item.contents }}
             </v-card-text>
           </v-card>
+          </v-hover>
         </v-col>
       </v-row>
     </v-container>
@@ -44,5 +51,13 @@
   }
 </script>
 <style>
-  @import "./../css/mycss.css";
+@import "./../css/mycss.css";
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+}
 </style>
