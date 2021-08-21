859125bff019fcf50e524c95c459604b473fecab
fkubota <rkkubotay@gmail.com>
2021-08-21 16:06:37 +0900

    [update] build

--- src/components/Aboutme.vue
+++ src/components/Aboutme.vue
@@ -5,13 +5,16 @@
 					<h1 class="contents-title">About Me.</h1>
 					<h2 class="contents-body">Fumihiro Kubota/久保田 史洋</h2>
 					<h4 class="contents-body">機械学習エンジニア</h4>
+					<h4 class="contents-body">KaggleExpert(銀4, 銅2)</h4>
           <p class="contents-body">
+          
           <br/>
           自慢できることは一つだけ "沖縄出身であること" です。<br/>
           バンドルカードの会社(株式会社カンム)で機械学習エンジニアをしています。<br/>
           学問/コーヒー/お酒/友達 を愛しています。<br/>
           早起きが趣味です。 <br/>
-          最近は機械学習とビジネスを繋ぐところに興味があります。
+          最近は機械学習とビジネスを繋ぐところに興味があります。 <br/>
+
           </p>
 					<v-row>
 
@@ -46,6 +49,30 @@
                 </v-btn>
               </template>
               <span>Speaker Deck</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon href='https://note.com/f_kubota' v-bind="attrs" v-on="on">
+                  <v-icon color='#cccccc'>mdi-file-outline</v-icon>
+                </v-btn>
+              </template>
+              <span>note</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon href='https://zenn.dev/fkubota' v-bind="attrs" v-on="on">
+                  <v-icon color='#cccccc'>mdi-alpha-z-circle</v-icon>
+                </v-btn>
+              </template>
+              <span>Zenn</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon href='https://qiita.com/fkubota' v-bind="attrs" v-on="on">
+                  <v-icon color='#cccccc'>mdi-alpha-q-circle</v-icon>
+                </v-btn>
+              </template>
+              <span>Qiita</span>
             </v-tooltip>
             <v-tooltip bottom>
               <template v-slot:activator="{ on, attrs }">
