f07254e1e50c50f7ec8f6f3982a4bf6fa8b5de73
fkubota <rkkubotay@gmail.com>
2021-08-21 11:48:01 +0900

    [update] build

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,9 +1,9 @@
 <template>
       <v-container class="contents">
         <h1 class="contents-title">Timeline/Bookshelf.</h1>
-        <v-row align="center" justify="center" class="fill-height">
+        <v-row align="center" justify="center">
 
-          <v-carousel v-model="model" show-arrows-on-hover cycle interval=3000>
+          <v-carousel v-model="model" show-arrows-on-hover cycle interval=3000 style="max-width:800px;">
             <v-carousel-item
                 v-for="(item,i) in items"
                 :key="i"
